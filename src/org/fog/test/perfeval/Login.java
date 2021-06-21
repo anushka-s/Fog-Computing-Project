4b7b369787adc5701c380293b69239a102713b37
AnnaSrivastava <ananyavsrivastava@gmail.com>
2021-06-21 08:19:16 +0530

    login form done

--- src/org/fog/test/perfeval/Login.java
+++ src/org/fog/test/perfeval/Login.java
@@ -3,24 +3,33 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Login{
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
         JFrame frame = new JFrame("JFrame Example");  
 		JButton blogin = new JButton("Login");
 		JPanel panel = new JPanel();
 		JTextField txuser = new JTextField(15);
 		JPasswordField pass = new JPasswordField(15);
+		JLabel label1 = new JLabel("IP Address");
+		JLabel label2 = new JLabel("MAC Address");
 
-		frame.setSize(300,200);
+		frame.setSize(350,200);
 		frame.setLocation(500,280);
 		panel.setLayout (null);
 
-		txuser.setBounds(70,30,150,20);
-		pass.setBounds(70,65,150,20);
+		txuser.setBounds(90,30,150,20);
+        label1.setBounds(10, 30, 100, 20);
+		pass.setBounds(90,65,150,20);
+		label2.setBounds(10,65, 100, 20);
 		blogin.setBounds(110,100,80,20);
 
 		panel.add(blogin);
 		panel.add(txuser);
 		panel.add(pass);
+		panel.add(label1);
+		panel.add(label2);
 
 		frame.getContentPane().add(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,14 +41,14 @@ blogin.addActionListener(new ActionListener() {
 public void actionPerformed(ActionEvent ae) {
 String puname = txuser.getText();
 String ppaswd = String.valueOf(pass.getPassword());
-if(puname.equals("test") && ppaswd.equals("12345"))
+if(puname.equals("192.168.1.10") && ppaswd.equals("fe80::3c94:b419:fc76:8fa0%20"))
 	{
 	StartApp regFace =new StartApp();
 	regFace.setVisible(true);
 	frame.dispose();
 	} 
 else {
-	JOptionPane.showMessageDialog(null,"Wrong Password / Username");
+	JOptionPane.showMessageDialog(null,"Wrong IP/MAC Address");
 	txuser.setText("");
 	pass.setText("");
 	txuser.requestFocus(); 
